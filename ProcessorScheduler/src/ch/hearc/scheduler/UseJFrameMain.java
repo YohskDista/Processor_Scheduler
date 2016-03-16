e216f15db2e2140fddfe3a97019795fa92ffac02
leonardodistasio <leonardo.distasio@he-arc.ch>
2016-03-16 14:37:08 +0100

    Workspace Git Init

---
+++ ProcessorScheduler/src/ch/hearc/scheduler/UseJFrameMain.java
@@ -0,0 +1,14 @@
+package ch.hearc.scheduler;
+
+public class UseJFrameMain 
+{
+	public static void main(String[] args)
+	{
+		main();
+	}
+	
+	public static void main()
+	{
+		new JFrameMain();
+	}
+}
